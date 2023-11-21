e23568f62095d0953210d55c8745cd2dd8944824
ahonanhin <yannelroy0@gmail.com>
2023-11-20 20:59:47 -0500

    modified:   text_to_speech_1.py

--- text_to_speech_1.py
+++ text_to_speech_1.py
@@ -1,49 +1,45 @@
-# importing libraries 
-import tkinter
+import tkinter as tk
 import pyttsx3
-import playsound
 
-def text_to_speech_converter():
+
+# using pyttsx3 module to convert text to speech
+def text_to_speech_converter(text_entry):
     text_speech = pyttsx3.init()
-    words = text_entry.get("1.0", END)
+    words = text_entry.get("1.0", tk.END)
     text_speech.say(words)
     text_speech.runAndWait()
-def reset():
+
+# reset the text entry
+def reset(text_entry):
     text_entry.delete("1.0", "end")
 
-# window
-from tkinter import *
-# declaring the window
-appWindow = Tk()
-# Creating the window title
-appWindow.title("Text-to-speech 1.0")
-# window dimensions
-appWindow.configure(width = 1600, height = 1200)
-# window colors
-appWindow.configure(bg = "lightblue")
-# App Title label
-main_title = tkinter.Label(appWindow, text = "Enter your text", 
-font = "Times 18", bg= "light blue").grid(row=0,column = 3)
+#
+def main():
+    # window
+    app_window = tk.Tk()
+    app_window.title("Text-to-speech 1.0")
+    app_window.configure(width=1600, height=1200, bg="lightblue")
 
-text_entry = tkinter.Text(appWindow, font= "Times 14")
-text_entry.grid(row=1, column=3)
+    main_title = tk.Label(app_window, text="Enter your text", font="Times 18", bg="light blue")
+    main_title.grid(row=0, column=3)
 
-# Creating the conversion button
-convert = tkinter.Button(appWindow, text = "Convert", bd= 5, bg = "light green",
-fg = "black", font = "Times",
- command= text_to_speech_converter)
-convert.grid(row = 8, column =1 )
+    text_entry = tk.Text(app_window, font="Times 14")
+    text_entry.grid(row=1, column=3)
 
-#creating the reset button
-reset_button = tkinter.Button(appWindow, text = "Clear" , bd ="10", bg = 'Light Green', 
-command = reset)
-reset_button.grid(row = 8, column = 3)
+    convert = tk.Button(app_window, text="Convert", bd=5, bg="light green", fg="black", font="Times",
+                        command=lambda: text_to_speech_converter(text_entry))
+    convert.grid(row=8, column=1)
 
-# Creating the exit Button
-exit_button = tkinter.Button(appWindow, text = "Exit", bd = "10",
- bg = "Light Green", command= appWindow.destroy)
-exit_button.grid(row = 8, column =10)
+    reset_button = tk.Button(app_window, text="Clear", bd="10", bg='Light Green',
+                             command=lambda: reset(text_entry))
+    reset_button.grid(row=8, column=3)
+
+    exit_button = tk.Button(app_window, text="Exit", bd="10", bg="Light Green", command=app_window.destroy)
+    exit_button.grid(row=8, column=10)
+
+    # infinite loop to run program until user exits
+    app_window.mainloop()
 
 
-appWindow.mainloop()
-
+if __name__ == "__main__":
+    main()
